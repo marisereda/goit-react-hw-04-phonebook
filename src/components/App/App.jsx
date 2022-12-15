0d63f73f60007b365c4fcdb33187b521fa7ff0e8
Marina Sereda <marina.gorb@gmail.com>
2022-12-15 18:34:12 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,7 +25,6 @@ export const App = () => {
 
   // --------------------------------
   useEffect(() => {
-    console.log(isFirstRender);
     if (isFirstRender.current) {
       isFirstRender.current = false;
       return;
